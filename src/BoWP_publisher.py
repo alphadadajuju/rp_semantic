4f9f2198758c1c83270ab9dc50af61183f293ac5
Albert C <albertclerigues@gmail.com>
2017-11-08 13:41:21 +0000

    Add debug output mode. Fix distance metric.

--- src/BoWP_publisher.py
+++ src/BoWP_publisher.py
@@ -20,14 +20,17 @@ if __name__ == '__main__':
     bowp_pub = rospy.Publisher("rp_semantic/bow_bowp_descriptors", BoWP, queue_size=5)
     rospy.sleep(1.0)
 
-    pub_msg(bowp_pub, 0, 2000 * sp.rand(3), 2000 * sp.rand(3))
-
-    pub_msg(bowp_pub, 0, [2000,2000,2000], [2000,2000,2000])
+    pub_msg(bowp_pub, 0, [1000,1000,1000], [1000,1000,1000])
 
     for i in range(10):
         pub_msg(bowp_pub, 0, 2000*sp.rand(3), 2000*sp.rand(3))
 
-    pub_msg(bowp_pub, 0, [2000,2000,2000], [2000,2000,2000])
+    pub_msg(bowp_pub, 0, [999,1001,1000], [999,1001,1000])
+
+    for i in range(10):
+        pub_msg(bowp_pub, 0, 2000*sp.rand(3), 2000*sp.rand(3))
+
+    pub_msg(bowp_pub, 0, [1000, 1000, 1000], [1000, 1000, 1000])
 
     rospy.sleep(0.5)
 
--- src/BoWPmatching.py
+++ src/BoWPmatching.py
@@ -6,6 +6,9 @@ import numpy.linalg as la
 import rospy
 from rp_semantic.msg import BoWP
 from threading import Thread, Lock
+from scipy.spatial.distance import cosine as cosine_dist
+
+DEBUG_MODE = False
 
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
@@ -23,45 +26,50 @@ class BoWPmatching:
         self.bowp_subscriber = rospy.Subscriber("rp_semantic/bow_bowp_descriptors", BoWP, self.descriptor_callback)
 
         self.t = -1 # Timestep indication
-        self.p = 1 # Frame margin for matching
+        self.p = 3 # Frame margin for matching min of 6
 
         ### Important variables
         # Descriptors are stored as (N_nodes x N_words) matrices
-        self.bow_descriptors = None
-        self.bowp_descriptors = None
+        self.bow_descriptors_list = []
+        self.bowp_descriptors_list = []
+
+        self.bow_descriptors = np.matrix([])
+        self.bowp_descriptors = np.matrix([])
 
         # Bayesian filtering for temporal coherence
-        self.prior_neighbourhood = 3
+        self.prior_neighbourhood = 1
         self.previous_loop_closure_event = -1
 
 
     def descriptor_callback(self, msg):
         self.mutex.acquire()
 
-        # TODO implement node_id update
-        # if self.bow_descriptors is not None and msg.node_id in range(self.bow_descriptors.shape[0]-1):
-        #     # Update case
-        #     pass
-        # else:
-
-        # Additions
-        print("Received descriptors", msg.bow, msg.bowp)
-
-        self.t += 1
-        if self.bow_descriptors is None:
-            self.bow_descriptors = np.matrix(msg.bow)
-            self.bowp_descriptors = np.matrix(msg.bowp)
+        # Todo modify message
+        if False and self.bow_descriptors is not None and msg.node_id >= 0:
+             # Update case
+             pass
         else:
-            self.bow_descriptors = np.append(self.bow_descriptors, np.matrix(msg.bow), axis=0)
-            self.bowp_descriptors = np.append(self.bowp_descriptors, np.matrix(msg.bowp), axis=0)
+            # Additions
+            if DEBUG_MODE:
+                print("Received descriptors", msg.bow, msg.bowp)
 
-            if self.bow_descriptors.shape[0] <= self.p:
-                self.mutex.release()
-                return
+            self.t += 1
 
-            self.previous_loop_closure_event = self.match_last_frame()
+            if self.bow_descriptors.size == 0:
+                self.bow_descriptors_list = [msg.bow]
+                self.bowp_descriptors_list = [msg.bowp]
+            else:
+                self.bow_descriptors_list.append(msg.bow)
+                self.bowp_descriptors_list.append(msg.bowp)
 
-        print("Loop closure at time " + str(self.t) + " is " + str(self.previous_loop_closure_event))
+            self.bow_descriptors = np.matrix(self.bow_descriptors_list)
+            self.bowp_descriptors = np.matrix(self.bowp_descriptors_list)
+
+            if self.bow_descriptors.shape[0] > self.p:
+                self.previous_loop_closure_event = self.match_last_frame()
+
+            if self.previous_loop_closure_event != -1:
+                print("Loop closure between " + str(self.t) + " and " + str(self.previous_loop_closure_event))
 
         self.mutex.release()
 
@@ -74,11 +82,11 @@ class BoWPmatching:
         n_i_bow = np.sum(self.bow_descriptors, axis=1)
         n_i_bowp = np.sum(self.bowp_descriptors, axis=1)
 
-        # n_w - count total number of words w in all nodes (sum descriptor columns (axis=0))
+        # n_w - count number of images containing w in all nodes (sum descriptor columns (axis=0))
         n_w_bow = np.sum(self.bow_descriptors, axis=0)
         n_w_bowp = np.sum(self.bowp_descriptors, axis=0)
 
-        # N - sum n_i vector
+        # N - total number of images seen so far
         N_bow = np.sum(n_i_bow)
         N_bowp = np.sum(n_i_bowp)
 
@@ -86,33 +94,39 @@ class BoWPmatching:
         bowp_tfidf = np.asmatrix(np.zeros(self.bowp_descriptors.shape))
         for i in range(0, self.bow_descriptors.shape[0]):
             bow_tfidf[i] = np.multiply(np.divide(self.bow_descriptors[i], n_i_bow[i]),
-                                       np.log((1.0 / N_bow) * n_w_bow + 0.000001))
+                                       np.log( N_bow / n_w_bow ))
             bowp_tfidf[i] = np.multiply(np.divide(self.bowp_descriptors[i], n_i_bowp[i]),
-                                        np.log((1.0 / N_bowp) * n_w_bowp + 0.000001))
+                                        np.log(N_bowp / n_w_bowp))
 
-        # ALL WRONG
+            #Normalize the tf-idf vectors
+            #bow_tfidf[i] = bow_tfidf[i]/np.sum(bow_tfidf[i])
+            #bowp_tfidf[i] = bow_tfidf[i]/np.sum(bowp_tfidf[i])
+
+        #print("tfidf", bow_tfidf)
+
         bow_similarity_scores = np.zeros(bow_tfidf.shape[0] - 1)
         bowp_similarity_scores = np.zeros(bowp_tfidf.shape[0] - 1)
         for i in range(0, bow_tfidf.shape[0] - 1):
-            bow_similarity_scores[i] = np.cos(angle_between(bow_tfidf[-1], bow_tfidf[i]))
-            bowp_similarity_scores[i] = np.cos(angle_between(bowp_tfidf[-1], bowp_tfidf[i]))
+            bow_similarity_scores[i] = 1.0/(cosine_dist(bow_tfidf[-1], bow_tfidf[i]) + 1E-12)
+            bowp_similarity_scores[i] = 1.0/(cosine_dist(bowp_tfidf[-1], bowp_tfidf[i]) + 1E-12)
 
+        if DEBUG_MODE:
+            print("Similarity scores", bow_similarity_scores)
 
         ### Compute likelihood
 
         # Update mean and stddev
         bow_similarity_mean = np.mean(bow_similarity_scores)
-        bow_similarity_stddev = np.std(bow_similarity_scores) + 0.000001
+        bow_similarity_stddev = np.std(bow_similarity_scores)
         bowp_similarity_mean = np.mean(bowp_similarity_scores)
-        bowp_similarity_stddev = np.std(bowp_similarity_scores) + 0.000001
-        # print(self.bow_similarity_mean, self.bow_similarity_stddev, self.bowp_similarity_mean, self.bowp_similarity_stddev)
+        bowp_similarity_stddev = np.std(bowp_similarity_scores)
 
         joint_likelihood = np.zeros(bow_similarity_scores.shape[0] + 1)
         for i in range(0, bow_similarity_scores.shape[0]):
             bow_likelihood, bowp_likelihood = 1, 1
-            if bow_similarity_scores[i] >= (bow_similarity_mean + bow_similarity_stddev):
+            if bow_similarity_scores[i] > (bow_similarity_mean + bow_similarity_stddev):
                 bow_likelihood = (bow_similarity_scores[i] - bow_similarity_stddev) / bow_similarity_mean
-            if bowp_similarity_scores[i] >= (bowp_similarity_mean + bowp_similarity_stddev):
+            if bowp_similarity_scores[i] > (bowp_similarity_mean + bowp_similarity_stddev):
                 bowp_likelihood = (bowp_similarity_scores[i] - bowp_similarity_stddev) / bowp_similarity_mean
 
             joint_likelihood[i] = bow_likelihood*bowp_likelihood
@@ -120,15 +134,19 @@ class BoWPmatching:
         # The likelihood of new node is encoded on the final entry
         joint_likelihood[-1] = ((bow_similarity_mean / bow_similarity_stddev) + 1) * \
                                 ((bowp_similarity_mean / bowp_similarity_stddev) + 1)
-        #print(self.joint_likelihood)
 
+        if(np.isnan(joint_likelihood[-1]) or np.isinf(joint_likelihood[-1])):
+            joint_likelihood[-1] = 1
+
+        if DEBUG_MODE:
+            print("Likelihood", joint_likelihood)
 
         ### Get posterior
         prior = self.compute_prior(joint_likelihood.shape[0], self.previous_loop_closure_event)
         posterior = np.multiply(joint_likelihood, prior)
 
-        #print("Likelihood: ", joint_likelihood)
-        print("Posterior: ", posterior)
+        if DEBUG_MODE:
+            print("Posterior: ", posterior)
 
         return np.argmax(posterior[:-1]) if np.amax(posterior[:-1]) > 0.5 else -1
 
@@ -139,12 +157,16 @@ class BoWPmatching:
             prior += 0.1/(self.t - self.p + 1)
             prior[-1] = 0.9
         else:
+            neigh_left = np.min([previous_loop_closure, self.prior_neighbourhood])
+            neigh_right = np.min([prior_size - previous_loop_closure,  self.prior_neighbourhood])
+
             gaussian_weights = gaussian(np.arange(-self.prior_neighbourhood, self.prior_neighbourhood, 1),
-                                        0, (self.prior_neighbourhood/2.0))
+                                        0, (self.prior_neighbourhood/0.5))
             gaussian_weights = 0.9*(gaussian_weights/np.sum(gaussian_weights))
 
-            prior[previous_loop_closure - self.prior_neighbourhood:
-                       previous_loop_closure + self.prior_neighbourhood] = gaussian_weights
+            gaussian_weights = gaussian_weights[(self.prior_neighbourhood - neigh_left):]
+
+            prior[previous_loop_closure - neigh_left: previous_loop_closure + neigh_right] = gaussian_weights
             prior[-1] = 0.1
 
         # print("prior", self.prior)
@@ -154,6 +176,8 @@ class BoWPmatching:
 
 if __name__ == '__main__':
     rospy.init_node("bowp_matching")
+
+    np.set_printoptions(suppress=True, precision=5)
 
     matcher = BoWPmatching()
     rospy.spin()
